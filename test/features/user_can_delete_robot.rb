8e7bd3a6852d63e64b3648b6daaf05a7469f791e
alirezaandersen <alireza.andersen@gmail.com>
2016-03-26 17:37:04 -0600

    webdesign

--- test/features/user_can_delete_robot.rb
+++ test/features/user_can_delete_robot.rb
@@ -12,18 +12,8 @@ class UserSeesAllRobotsTest < FeatureTest
 
     robo = robot_manager.all.last
     visit '/robots/%d' % [robo.id]
-    # visit '/robots/new'
-    # fill_in 'robot[name]', with: 'Ali'
-    # fill_in 'robot[city]', with: 'Denver'
-    # fill_in 'robot[state]', with: 'Colorado'
-    # fill_in 'robot[birthdate]', with: '1980-01-06'
-    # fill_in 'robot[date_hired]', with: '2015-12-03'
-    # fill_in 'robot[department]', with: 'Student @turing'
-    # fill_in 'robot[avatar]', with: 'https://cbsmancave.files.wordpress.com/2013/04/subwooferbot-thinkstock.jpg?w=642'
-    # visit '/robot'
-    # enter the password
 
-binding.pry
+    binding.pry
     find('#%d' % [robo.id]).click
     assert_equal robot_manager.all.length, robot_count-1
 
